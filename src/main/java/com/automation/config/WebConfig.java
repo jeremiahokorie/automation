0108f45bdba23eeb1758150e5ebd1ab96df60d0c
jeremiah.imo <jeremiah.imo@etranzact.com>
2025-05-28 16:32:13 +0100

    updated code

--- src/main/java/com/automation/config/WebConfig.java
+++ src/main/java/com/automation/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5174")
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
