ba334c7585feb7669d63bbb8b29d91a5aeba35b9
jeremiah.imo <jeremiah.imo@etranzact.com>
2025-06-16 18:35:31 +0100

    updated code

--- src/main/java/com/automation/core/global/model/User.java
+++ src/main/java/com/automation/core/global/model/User.java
@@ -52,11 +52,11 @@ public class User implements UserDetails, Serializable {
 //    private Roles role;
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id")
-    )
+//    @JoinTable(
+//            name = "user_roles",
+//            joinColumns = @JoinColumn(name = "user_id"),
+//            inverseJoinColumns = @JoinColumn(name = "role_id")
+//    )
     private Set<Roles> roles = new HashSet<>();
 
     @Override
